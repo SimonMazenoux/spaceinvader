febb05dce8774ccc3fce0ef54bab3db2b5c2335b
Simon Mazenoux <simon.mazenoux@gmail.com>
2019-04-08 17:16:14 +0200

    Ajout de l'HorsEspaceJeuException

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,5 +1,7 @@
 package fr.unilim.iut.spaceinvaders;
 
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvaders {
 	
 	private static final char MARQUE_FIN_LIGNE = '\n';
@@ -60,8 +62,14 @@ public class SpaceInvaders {
 		return vaisseau!=null;
 	}
 	
+
 	public void positionnerUnNouveauVaisseau(int x, int y) {
-        this.vaisseau = new Vaisseau (x,y);
+		
+		if (x >= longueur)
+			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
+		
+		vaisseau = new Vaisseau(x, y);
+
 	}
 	
 }
--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -3,6 +3,8 @@ package fr.unilim.iut.spaceinvaders;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvadersTest {
 	
 	@Test
@@ -36,5 +38,11 @@ public class SpaceInvadersTest {
 		"...............\n" + 
 		"...............\n" + 
 		".......V.......\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+	}
+	
+	@Test(expected = HorsEspaceJeuException.class)
+	public void test_unNouveauVaisseauEstPositionneHorsEspaceJeuTropADroite_UneExceptionEstLevee() throws Exception {
+		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		spaceinvaders.positionnerUnNouveauVaisseau(15,9);
 	}
 }
