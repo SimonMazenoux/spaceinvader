d8cafc9bddd58b83952841db11df9d7be2512490
Simon Mazenoux <simon.mazenoux@gmail.com>
2019-05-03 08:45:29 +0200

    positionner un vaisseau avec une dimension

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/DebordementEspaceJeuException.java
@@ -0,0 +1,8 @@
+package fr.unilim.iut.spaceinvaders;
+
+public class DebordementEspaceJeuException extends RuntimeException {
+	
+	public DebordementEspaceJeuException(String message) {
+	    super(message);
+    }
+}
