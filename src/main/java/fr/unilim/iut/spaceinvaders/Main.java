e3c4b24d91e419578ea2fea03da5f156af9829a7
Simon Mazenoux <simon.mazenoux@gmail.com>
2019-05-06 18:53:38 +0200

    Intégration du moteur graphique.

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Main.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.spaceinvaders;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,17 +1,13 @@
 package fr.unilim.iut.spaceinvaders;
 
+
+import fr.unilim.iut.spaceinvaders.moteurjeu.Commande;
+import fr.unilim.iut.spaceinvaders.moteurjeu.Jeu;
 import fr.unilim.iut.spaceinvaders.utils.DebordementEspaceJeuException;
 import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
-public class SpaceInvaders {
+public class SpaceInvaders implements Jeu{
 
-	private static final int LONGUEUR_MINIMUM = 0;
-
-	private static final char MARQUE_FIN_LIGNE = '\n';
-
-	private static final char MARQUE_VIDE = '.';
-
-	private static final char MARQUE_VAISSEAU = 'V';
 
 	int longueur;
 	int hauteur;
@@ -28,7 +24,7 @@ public class SpaceInvaders {
 			for (int x = 0; x < longueur; x++) {
 				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
-			espaceDeJeu.append(MARQUE_FIN_LIGNE);
+			espaceDeJeu.append(Constante.MARQUE_FIN_LIGNE);
 		}
 		return espaceDeJeu.toString();
 	}
@@ -36,9 +32,9 @@ public class SpaceInvaders {
 	private char recupererMarqueDeLaPosition(int x, int y) {
 		char marque;
 		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
-			marque = MARQUE_VAISSEAU;
+			marque = Constante.MARQUE_VAISSEAU;
 		else
-			marque = MARQUE_VIDE;
+			marque = Constante.MARQUE_VIDE;
 		return marque;
 	}
 
@@ -46,7 +42,7 @@ public class SpaceInvaders {
 		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
 	}
 
-	private boolean aUnVaisseau() {
+	public boolean aUnVaisseau() {
 		return vaisseau != null;
 	}
 
@@ -80,9 +76,33 @@ public class SpaceInvaders {
 	}
 
 	public void deplacerVaisseauVersLaGauche() {
-		if (vaisseau.abscisseLaPlusAGauche() > LONGUEUR_MINIMUM) {
+		if (vaisseau.abscisseLaPlusAGauche() > Constante.LONGUEUR_MINIMUM) {
 			vaisseau.seDeplacerVersLaGauche();
 		}
 	}
 
+	@Override
+	public void evoluer(Commande commandeUser) {
+		if (commandeUser.gauche) {
+			deplacerVaisseauVersLaGauche();
+		}
+		if (commandeUser.droite) {
+			deplacerVaisseauVersLaDroite();
+		}
+	}
+
+	@Override
+	public boolean etreFini() {
+		return false;
+	}
+
+	public Vaisseau recupererVaisseau() {
+		return this.vaisseau;
+	}
+	
+	public void initialiserJeu() {
+	    Position positionVaisseau = new Position(this.longueur/2,this.hauteur-1);
+	    Dimension dimensionVaisseau = new Dimension(Constante.VAISSEAU_LONGUEUR, Constante.VAISSEAU_HAUTEUR);
+	    positionnerUnNouveauVaisseau(dimensionVaisseau, positionVaisseau);
+    }
 }
